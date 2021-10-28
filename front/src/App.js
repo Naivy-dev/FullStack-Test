64e6dd8cad34f095351b54254631924f5c5b555c
Arthur Brites <arth.brites@gmail.com>
2021-10-27 22:45:03 -0300

    finished

--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,9 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Card } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ToggleButton from "./components/ToggleButton";
-import { useDispatch } from "react-redux";
-import { createInfo } from "./actions/info";
 
 function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(createInfo());
-    return;
-  }, [dispatch]);
-
   return (
     <div className="App">
       <Card>
--- front/src/components/MainResponse.js
+++ front/src/components/MainResponse.js
@@ -5,38 +5,50 @@ import { useSelector } from "react-redux";
 export default function MainResponse(props) {
   const info = useSelector((state) => state.info);
   console.log(info);
+
+  const refreshPage = () => {
+    window.location.reload(false);
+  };
+
+  const goBack = () => {
+    props.setMode.bind(null, "form");
+    refreshPage();
+  };
+
   return (
     <Card style={{ width: "40rem" }}>
       <Card.Body>
         <Card.Title>
           <h2>Informações</h2>
         </Card.Title>
-        <ListGroup>
-          <ListGroupItem>
-            <Card.Title>Nome</Card.Title>
-            {info.nome}
-          </ListGroupItem>
-          <ListGroupItem>
-            <Card.Title>Endereço</Card.Title>
-            <ListGroup>
-              <ListGroupItem>CEP:</ListGroupItem>
-              <ListGroupItem>Logradouro: </ListGroupItem>
-              <ListGroupItem>Bairro:</ListGroupItem>
-              <ListGroupItem>Localidade: </ListGroupItem>
-              <ListGroupItem>UF: </ListGroupItem>
-            </ListGroup>
-          </ListGroupItem>
-          <ListGroupItem>
-            <Card.Title>Renda per Capita</Card.Title>
-            {info.rendaPerCapita}
-          </ListGroupItem>
-        </ListGroup>
+        {info.length > 0 && (
+          <ListGroup>
+            <ListGroupItem>
+              <Card.Title>Nome</Card.Title>
+              {info[0].nome !== undefined && info[0].nome}
+            </ListGroupItem>
+            <ListGroupItem>
+              <Card.Title>Endereço</Card.Title>
+              <ListGroup>
+                <ListGroupItem>CEP: {info[0].endereco.cep}</ListGroupItem>
+                <ListGroupItem>
+                  Logradouro: {info[0].endereco.logradouro}
+                </ListGroupItem>
+                <ListGroupItem>Bairro: {info[0].endereco.bairro}</ListGroupItem>
+                <ListGroupItem>
+                  Localidade: {info[0].endereco.localidade}
+                </ListGroupItem>
+                <ListGroupItem>UF: {info[0].endereco.uf}</ListGroupItem>
+              </ListGroup>
+            </ListGroupItem>
+            <ListGroupItem>
+              <Card.Title>Renda per Capita</Card.Title>
+              R${info[0].rendaPerCapita}
+            </ListGroupItem>
+          </ListGroup>
+        )}
         <Col style={{ marginTop: "10px" }}>
-          <Button
-            variant="primary"
-            type="submit"
-            onClick={props.setMode.bind(null, "form")}
-          >
+          <Button variant="primary" type="submit" onClick={goBack}>
             Voltar
           </Button>
         </Col>
